0ed671ceae206895101280ab72cce007465395c8
Yun1903 <alperenyuncbae@gmail.com>
2023-06-08 21:54:12 +0300

    son duzenlemeler

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
- console.log("calisiyor mu?");
+ const searchForm = document.querySelector(".search-form");
+ const cartItem = document.querySelector(".cart-items-container");
+ const navbar = document.querySelector(".navbar");
+
+ // buttons
+
+ const searchBtn = document.querySelector("#search-btn");
+ const cartBtn = document.querySelector("#cart-btn");
+ const gundemBtn = document.querySelector("#gundem-btn");
+
+
+ searchBtn.addEventListener("click", function(){
+    searchForm.classList.toggle("active");
+    document.addEventListener("click", function(e){
+        if(
+            !e.composedPath().includes(searchBtn) && !e.composedPath().includes(searchForm)
+            ){
+            searchForm.classList.remove("active");
+        }
+
+    })
+ });
+
+ cartBtn.addEventListener("click", function(){
+    cartItem.classList.toggle("active");
+    document.addEventListener("click", function(e){
+        if(
+            !e.composedPath().includes(cartBtn) && !e.composedPath().includes(cartItem)
+            ){
+                cartItem.classList.remove("active");
+        }
+
+    })
+ });
+
+    gundemBtn.addEventListener("click", function(){
+    navbar.classList.toggle("active");
+    document.addEventListener("click", function(e){
+        if(
+            !e.composedPath().includes(gundemBtn) && !e.composedPath().includes(navbar)
+            ){
+            navbar.classList.remove("active");
+        }
+
+    })
+ });
